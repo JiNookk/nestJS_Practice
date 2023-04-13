4ab9ea9755270aabfb0cc94b28ad2b6e361560a4
jingwook <qnfdkf153@gmail.com>
2023-04-13 15:32:02 +0900

    nestJS CRUD

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,7 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
+} from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Board, BoardStatus } from './boards.model';
+import { CreateBoardDto } from './create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
-  constructor(private boardsService: BoardsService) {}
+    constructor(private boardsService: BoardsService) {}
+
+    @Get('/')
+    getAllBoards(): Board[] {
+        return this.boardsService.getAllBoards();
+    }
+
+    @Post('/')
+    createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+        return this.boardsService.createBoard(createBoardDto);
+    }
+
+    @Get('/:id')
+    getBoardById(@Param('id') id: string): Board {
+        return this.boardsService.getBoardById(id);
+    }
+
+    @Patch('/:id/status')
+    updateBoardStatusById(
+        @Param('id') id: string,
+        @Body('status') status: BoardStatus,
+    ): Board {
+        return this.boardsService.updateStatusById(id, status);
+    }
+
+    @Delete('/:id')
+    deleteBoardById(@Param('id') id: string): Board {
+        return this.boardsService.deleteBoardById(id);
+    }
 }
